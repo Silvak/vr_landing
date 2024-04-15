98f55d135640f896e94d985f1a8adda850c18482
silvak <silvak.jeg@gmail.com>
2024-04-15 10:58:55 -0400

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="">Title</h1>
+      <Button>Text</Button>
+    </div>
+  );
+}
+
+export default App;
