157c91f75e3fde26f746d076daeb27586be0d275
silvak <silvak.jeg@gmail.com>
2024-05-27 08:05:28 -0400

    change logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import logo from "@/assets/brand/logoWhite.svg";
 
 function Header() {
   return (
-    <head className="flex justify-center w-full h-[80px] fixed top-0 z-[1000] backdrop-blur-lg border-b border-white/10">
+    <head className="flex justify-center w-full h-[88px] fixed top-0 z-[1000] backdrop-blur-lg border-b border-white/10">
       <div className="grid grid-cols-2 md:grid-cols-3 max-w-[1300px] px-4 md:px-0 w-full items-center">
         <div className="text-xl font-bold text-white">
           <a href="/">
-            <img src={logo} alt="" className="h-[48px]" />
+            <img src={logo} alt="" className="h-[80px]" />
           </a>
         </div>
 
